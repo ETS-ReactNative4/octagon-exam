ea6adc154c55b364946b2270d3a4625df4d2aa34
Nowshad <nowshad.club@gmail.com>
2018-02-12 22:21:17 +0600

    percentage correct

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -14,12 +14,22 @@ export default class ScoreBoard extends Component {
         return questions.filter(question => !question.answerCorrect && question.selectedOption != null).length;
     }
 
+    percentageCorrect(questions){
+        const correct = this.correctCount(questions);
+        if(correct === 0){
+            return 0;
+        }
+        return (correct/(correct + this.wrongCount(questions))) * 100;
+    }
+
     render(){
+        const {questions} = this.props;
        return (
         <div>
+            <h1>{this.percentageCorrect(questions)}%</h1>
             <p><strong>Total Questions:</strong> {this.props.questions.length}</p>
-            <p><strong>Wrong:</strong> {this.wrongCount(this.props.questions)}</p>
-            <p><strong>Correct:</strong> {this.correctCount(this.props.questions)}</p>
+            <p><strong>Wrong:</strong> {this.wrongCount(questions)}</p>
+            <p><strong>Correct:</strong> {this.correctCount(questions)}</p>
         </div>
        )
     }
